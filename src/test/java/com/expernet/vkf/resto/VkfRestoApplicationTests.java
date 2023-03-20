a19dc4cb2dd291c97455941c51582a3fb9caf9fe
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2023-03-20 15:32:26 +0400

    Initialisation du projet

---
+++ src/test/java/com/expernet/vkf/resto/VkfRestoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expernet.vkf.resto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkfRestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
