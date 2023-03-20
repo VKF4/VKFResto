a8947ad1852be7c2ffd42c780b4600633f915720
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2023-03-20 22:39:38 +0400

    creation du repository Plat

---
+++ src/main/java/com/expernet/vkf/resto/repository/PlatRepository.java
@@ -0,0 +1,9 @@
+package com.expernet.vkf.resto.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.expernet.vkf.resto.model.Plat;
+
+public interface PlatRepository extends CrudRepository<Plat, Long>{
+    
+}
