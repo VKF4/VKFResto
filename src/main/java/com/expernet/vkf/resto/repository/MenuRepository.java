0b4b28bdd960a8ed5b7423e6a1c1db6ef889bc22
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2023-03-20 22:38:30 +0400

    creation du repository Menu

---
+++ src/main/java/com/expernet/vkf/resto/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.expernet.vkf.resto.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.expernet.vkf.resto.model.Menu;
+
+public interface MenuRepository extends CrudRepository<Menu, Long>{
+    
+}
