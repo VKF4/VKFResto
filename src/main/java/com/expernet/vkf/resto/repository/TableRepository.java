ead643f82b6910a0c72016ef13d1ad594eaccbd2
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2023-03-20 22:40:52 +0400

    creation du repository Table

---
+++ src/main/java/com/expernet/vkf/resto/repository/TableRepository.java
@@ -0,0 +1,9 @@
+package com.expernet.vkf.resto.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.expernet.vkf.resto.model.Table;
+
+public interface TableRepository extends CrudRepository<Table, Long>{
+    
+}
