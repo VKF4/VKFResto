5c77eb91fddf8aa33619dc86bccbe79482a65a84
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2023-03-20 22:42:04 +0400

    creation du repository TypePlat

---
+++ src/main/java/com/expernet/vkf/resto/repository/TypePlatRepository.java
@@ -0,0 +1,9 @@
+package com.expernet.vkf.resto.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.expernet.vkf.resto.model.TypePlat;
+
+public interface TypePlatRepository extends CrudRepository<TypePlat, Long> {
+    
+}
