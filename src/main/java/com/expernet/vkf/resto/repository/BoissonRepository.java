493e6c7080dbef540c1b413659a6f162c30a78f2
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2023-03-20 22:36:19 +0400

    creation du repository Boisson

---
+++ src/main/java/com/expernet/vkf/resto/repository/BoissonRepository.java
@@ -0,0 +1,9 @@
+package com.expernet.vkf.resto.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.expernet.vkf.resto.model.Boisson;
+
+public interface BoissonRepository extends CrudRepository<Boisson, Long>{
+    
+}
