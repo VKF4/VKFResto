382ee06a5b2ee17d3dcfdee36eb84b2cda9bb8ba
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2023-03-23 11:05:49 +0400

    creation de la classe websecurity

---
+++ src/main/java/com/expernet/vkf/resto/security/WebSecurity.java
@@ -0,0 +1,26 @@
+package com.expernet.vkf.resto.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurity {
+
+	 	@Autowired
+	    private JwtRequestFilter jwtRequestFilter;
+	
+	 	@Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http.csrf().disable()
+	            .authorizeHttpRequests((authz) -> authz
+				.requestMatchers("/register", "/login").permitAll()
+	            .anyRequest().authenticated()
+	        );
+	            
+	        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	        return http.build();
+	    }
+}
